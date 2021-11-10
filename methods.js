8e7e0aa5640f22b43aaa4c2bc1e33f40e4ce3c88
Peter Opiyo <peter.opiyo@student.moringaschool.com>
2021-11-10 07:53:19 +0300

    week2a

---
+++ methods.js
@@ -0,0 +1,1 @@
+const mpesaMessage = "XXXYYYZZZ5 Confirmed. Ksh1,030.00 sent to MSEE FULANI HAPO +254722222222 on 23/4/21 at 2:38 PM. New M-PESA balance is Ksh7. Transaction cost, Ksh22.00. Amount you can transact within the day is 297,420.00. SAFARICOM ONLY CALLS YOU FROM 0722000000. To reverse, forward this message to 456.";
